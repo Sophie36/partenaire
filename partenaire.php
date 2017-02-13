9515063878e5cc38c826c5583b2030af225a6692
slemaire <slemaire@E117-03.CDF36.local>
2017-02-13 10:52:54 +0100

    avancement manipulation avec objets

--- partenaire.php
+++ partenaire.php
@@ -1,12 +1,29 @@
 <?php
 class Partenaire {
-   //Déclarer des propriétés
-    private $numId;
-    private $nom;
-   function __construct(){
-     echo"Toto";  
-} 
+   
+    private $numId;//Déclarer des propriétés
+    private $nom;//Déclarer des propriétés
+    
+  public function __construct(){//constructeur
+    
+       $this->numId="Inconnu";//initialise les propriétés
+       //echo"Toto";
+       $this->nom="Pas de nom";//initialise les propriétés
+       //echo"Toto";
+     
+   //public function afficher(){
+       //$nom="Toto fils";
+       //echo $this->nom;
+   //}
+  }     
+  public function setnumId($id){
+      $this->numId=$id;
+  }
 }
 $p1= new Partenaire();
-/*echo*/ //$p1->nom="Toto";
+//echo $p1->nom="Toto";
+
+$p1->setnumId(188);
+var_dump($p1);//affiche mes propriétés
+
 ?>
