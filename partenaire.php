6c1bd5723927f2753a5ab27578ad709b5d232786
slemaire <slemaire@E117-03.CDF36.local>
2017-02-13 11:54:37 +0100

    manipulation avec objets

--- partenaire.php
+++ partenaire.php
@@ -10,20 +10,43 @@ class Partenaire {
        //echo"Toto";
        $this->nom="Pas de nom";//initialise les propriétés
        //echo"Toto";
-     
+  } 
+  
    //public function afficher(){
        //$nom="Toto fils";
        //echo $this->nom;
    //}
-  }     
-  public function setnumId($id){
-      $this->numId=$id;
-  }
+   
+  //public function setnumId($id){
+      //$this->numId=$id;
+  //}
+  
+   public function afficher(){
+       echo"le numId est $this->numId</br>";
+       echo "Le nom du partenaire est $this->nom";
+   }    
+  
+
 }
+class Artiste Extends Partenaire {
+    private $telBooker;
+    function Artiste(){
+        $this->__construct();
+        $this->telBooker="aucun";
+    }
+        function afficher(){
+            echo"Le tel du booker est $this->telBooker";
+    }
+}
+
 $p1= new Partenaire();
 //echo $p1->nom="Toto";
 
-$p1->setnumId(188);
+//$p1->setnumId(188);
+
+$p2= new Artiste();
+$p2->afficher();
+
 var_dump($p1);//affiche mes propriétés
 
 ?>
