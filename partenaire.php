294df02e28388794b5e4a032a64db8083c7d2f55
slemaire <slemaire@E117-03.CDF36.local>
2017-02-13 10:19:20 +0100

    première manipulation avec objets

---
+++ partenaire.php
@@ -0,0 +1,12 @@
+<?php
+class Partenaire {
+   //Déclarer des propriétés
+    private $numId;
+    private $nom;
+   function __construct(){
+     echo"Toto";  
+} 
+}
+$p1= new Partenaire();
+/*echo*/ //$p1->nom="Toto";
+?>
